4ed789732b08e6e57d77c44e58fda9a35e00b57a
Ali Mohsen <ali_m_2001@hotmail.com>
2023-02-17 19:11:16 +0200

    added mobile compatibility

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let container = document.getElementsByClassName('contacts')[0];
-let empty = document.getElementsByClassName('empty')[0]
+let empty = document.getElementsByClassName('empty')[0];
 
 let addContact = function() {
     if (document.forms["myForm"]["name"].value == "" || document.forms["myForm"]["phone"].value == "") {
@@ -15,8 +15,8 @@ let addContact = function() {
     contact.setAttribute("class", "contact");
     close.setAttribute("class", "close");
     
-    n = document.getElementsByClassName('contact').length
-    close.setAttribute("onclick", "deleteContact("+n+")")
+    n = document.getElementsByClassName('contact').length;
+    close.setAttribute("onclick", "deleteContact("+n+")");
     close.innerText = "X";
     
     name.innerText = document.forms["myForm"]["name"].value;
@@ -34,7 +34,7 @@ let deleteContact = function(i) {
 
     contacts = document.getElementsByClassName('contact');
     for (i = 0; i < contacts.length; i++) {
-        contacts[i].children[2].setAttribute("onclick", "deleteContact("+i+")")
+        contacts[i].children[2].setAttribute("onclick", "deleteContact("+i+")");
     }
 
     if (i == 0) {
